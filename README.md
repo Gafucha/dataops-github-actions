05aa40c45d89ee380a7392e163c64b6f46c7407d
Gafucha <Gafucha95@gmail.com>
2025-06-10 17:09:07 -0300

    Correções

--- README.md
+++
@@ -1,2 +0,0 @@
-# dataops-github-actions
-Aula pipeline
--- app/conecta_postgres.py
+++ app/conecta_postgres.py
@@ -4,42 +4,25 @@ from flask import Flask
 from flask_restx import Api, Resource
 
 app = Flask(__name__)
-api = Api(app, version='1.0', title='Alunos API',
-          description='API para cadastro e consulta de alunos',
+api = Api(app, version='1.0', title='Atividade 3',
+          description='Atividade 3',
           doc='/swagger')
 
-ns = api.namespace('alunos', description='Operações relacionadas a alunos')
-
-def get_connection():
-    conn = psycopg2.connect(
-        host="db",
-        database="postgres",
-        user="postgres",
-        password="senha123"
-    )
-    return conn
+ns = api.namespace('Soma', description='post com a soma de 2 valores numéricos')
 
 @ns.route('/')
-class AlunosList(Resource):
-    def get(self):
-        """Lista todos os alunos"""
-        time.sleep(5)  # Espera o banco iniciar
-        conn = get_connection()
-        cur = conn.cursor()
-        cur.execute("""
-            CREATE TABLE IF NOT EXISTS alunos (
-                id SERIAL PRIMARY KEY,
-                nome VARCHAR(50)
-            );
-        """)
-        conn.commit()
-        cur.execute("INSERT INTO alunos (nome) VALUES ('Maria'), ('João') ON CONFLICT DO NOTHING;")
-        conn.commit()
-        cur.execute("SELECT * FROM alunos;")
-        alunos = cur.fetchall()
-        cur.close()
-        conn.close()
-        return {"alunos": [{"id": aluno[0], "nome": aluno[1]} for aluno in alunos]}
+class soma(Resource):
+    def postsoma(varx, vary):
+        svolta = str("soma: " + (varx + vary))
+        return svolta
+
+ms = api.namespace('Mult', description='post com a multiplicação de 2 valores numéricos')
+
+@ms.route('/mult')
+class mult(Resource):
+    def postmult(vara, varb):
+        mvolta = str("Multiplicação: " + (vara + varb))
+        return mvolta
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
